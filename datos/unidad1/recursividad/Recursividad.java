0b65264b9fc66622f9ee52e44b9fcf082ac8cad4
inkfoxy <inkfoxy74@gmail.com>
2026-09-24 15:53:02 -0600

    saludo con nombre

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -1,18 +1,17 @@
 package datos.unidad1.recursividad;
 public class Recursividad {
-
-        public static void saludo(int total) {
+       	public static void Saludo(int total, String nombre) {
+        
+        if(total <= 0)
+                return;
+        else{
+             	System.out.println("Hola " + nombre);
+                Saludo(total-1,nombre);
+        }
 	
-	if(total <= 0)
-		return;
-	else{
-		System.out.println("Hola");
-		saludo(total-1);
-	}
+        }
 	
-	}
-	
-	public static void main(String[] args){
-		saludo(10);
-	}
+        public static void main(String[] args){
+                Saludo(10, "ricardo");
+        }
 }
