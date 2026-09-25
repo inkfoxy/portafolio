cd88968cd4205a99bfd0b794c3d7d0fcb1e3ef22
inkfoxy <inkfoxy74@gmail.com>
2026-09-25 14:43:37 -0600

    se aggrego la funcion suma

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -2,12 +2,12 @@ package datos.unidad1.recursividad;
 public class Recursividad {
 	public static void Saludo(int total, String nombre) {
 	
-        if(total <= 0)
-                return;
-        else{
-             	System.out.println("Hola " + nombre);
-       	        Saludo(total-1,nombre);
-        }
+	        if(total <= 0)
+                	return;
+        	else{
+             		System.out.println("Hola " + nombre);
+       	        	Saludo(total-1,nombre);
+        	}
 	
         }
 
@@ -19,9 +19,23 @@ public class Recursividad {
                        	cuentaRegresiva(n-1);
 	       	}
        	}
-	
+
+	public static void sumaRecursiva(int[] datos, int longitud, int suma) {
+
+		if (longitud < 0) {
+			System.out.println("el resultado es: " + suma);
+			return;
+		}else{
+			suma = suma + datos[longitud];
+			sumaRecursiva(datos, longitud -1, suma);
+		}
+	}
         public static void main(String[] args){
                 //Saludo(10, "ricardo");
-		cuentaRegresiva(100);
+		//cuentaRegresiva(100);
+		int[] numeros = {20,20,20,7};
+		sumaRecursiva(numeros, numeros.length -1, 0);
+
         }
 }
+
